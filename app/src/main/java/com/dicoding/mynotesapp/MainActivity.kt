a20f05b196cb7988d60809a0c549151a40ba45ea
zaidanyh <zaidan.nganjuk1122@gmail.com>
2020-07-16 17:10:39 +0700

    initial commit

---
+++ app/src/main/java/com/dicoding/mynotesapp/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.dicoding.mynotesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.mynotesapp.adapter.NoteAdapter
+import com.dicoding.mynotesapp.db.NoteHelper
+import com.dicoding.mynotesapp.entity.Note
+import com.dicoding.mynotesapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: NoteAdapter
+    private lateinit var noteHelper: NoteHelper
+
+    companion object {
+        private const val EXTRA_STATE = "extra_state"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Notes"
+
+        rv_note.layoutManager = LinearLayoutManager(this)
+        rv_note.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        rv_note.adapter = adapter
+
+        fab_add.setOnClickListener {
+            startActivityForResult(Intent(this, NoteAddUpdateActivity::class.java), NoteAddUpdateActivity.REQUEST_ADD)
+        }
+
+        noteHelper = NoteHelper.getInstance(applicationContext)
+        noteHelper.open()
+
+        if (savedInstanceState == null) {
+            loadNotesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            when(requestCode) {
+                NoteAddUpdateActivity.REQUEST_ADD -> if (resultCode == NoteAddUpdateActivity.RESULT_ADD) {
+                    val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+
+                    adapter.addItem(note)
+                    rv_note.smoothScrollToPosition(adapter.itemCount -1)
+
+                    showSnackbarMessage("Satu Item berhasil ditambahkan")
+                }
+                NoteAddUpdateActivity.REQUEST_UPDATE ->
+                    when(resultCode) {
+                        NoteAddUpdateActivity.RESULT_UPDATE -> {
+
+                            val note = data.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE)
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+
+                            adapter.updateItem(position, note)
+                            rv_note.smoothScrollToPosition(position)
+
+                            showSnackbarMessage("Satu item berhasil diubah")
+                        }
+                        NoteAddUpdateActivity.RESULT_DELETE -> {
+                            val position = data.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0)
+                            adapter.removeItem(position)
+                            showSnackbarMessage("Satu item berhasil dihapus")
+                        }
+                    }
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listNotes)
+    }
+
+    private fun loadNotesAsync() {
+        GlobalScope.launch(Dispatchers.Main) {
+            progressBar.visibility = View.VISIBLE
+            val deferresNotes = async(Dispatchers.IO) {
+                val cursor = noteHelper.queryAll()
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+            progressBar.visibility = View.INVISIBLE
+            val notes = deferresNotes.await()
+            if (notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackbarMessage("Tidak Data saat ini")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        noteHelper.close()
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(rv_note, message, Snackbar.LENGTH_SHORT).show()
+    }
+}
